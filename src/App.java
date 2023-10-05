82f8c6a8e2114c488fff1713c7a5a5cddfa7bdf2
Luis Alejandro Cruz Inzunza <cruzalejandro113@gmail.com>
2023-10-05 09:39:39 -0600

    Funcion Resta generada

--- src/App.java
+++ src/App.java
@@ -2,4 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public int Resta(int minuendo, int sustraendo) {
+        int diferencia = minuendo - sustraendo;
+        return diferencia;
+    }
 }
