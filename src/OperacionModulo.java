e0f2266df50ce6f95ec73bf28ba56fb40549bd73
danieln <danms0171@gmail.com>
2023-10-05 18:26:56 -0600

    feat: Añade funcionalidad módulo.
    Implementa la funcionalidad de módulo al proyecto.
    
    closes issue #5

---
+++ src/OperacionModulo.java
@@ -0,0 +1,10 @@
+public class OperacionModulo {
+
+    public int modulo(int numeroA, int numeroB){
+        int valorModulo;
+        valorModulo = numeroA % numeroB;
+
+        return valorModulo;
+    }
+    
+}
