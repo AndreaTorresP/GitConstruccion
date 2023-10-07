845616d8cd31fb1085dca4159345c1cca00f4ec2
Luis Alejandro Cruz Inzunza <cruzalejandro113@gmail.com>
2023-10-07 13:39:35 -0600

    Funcion Resta generada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -13,14 +13,27 @@ public class Calculadora {
         valorTerminal.close();
 
         double resultadoOperacion = 0;
-        if(operacion.equals("suma"));
-        else if(operacion.equals("resta"));
-        else if(operacion.equals("multiplicacion"));
-        else if(operacion.equals("division"));
-        else if(operacion.equals("modulo"));
-        else if(operacion.equals("potencia"));
-        else System.out.println("Operacion no encontrada");
+        if (operacion.equals("suma"))
+            ;
+        else if (operacion.equals("resta")) {
+            resultadoOperacion = Resta(primerNum, segundoNum);
+        } else if (operacion.equals("multiplicacion"))
+            ;
+        else if (operacion.equals("division"))
+            ;
+        else if (operacion.equals("modulo"))
+            ;
+        else if (operacion.equals("potencia"))
+            ;
+        else
+            System.out.println("Operacion no encontrada");
 
         System.out.println(resultadoOperacion);
     }
+
+    public static double Resta(double minuendo, double sustraendo) {
+        double diferencia = minuendo - sustraendo;
+        return diferencia;
+    }
+
 }
