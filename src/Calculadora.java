9c4ce13893b1bc920731680ab48009f2f2fbe0d7
AndreaTorresP <A18003912@alumnos.uady.mx>
2023-10-07 20:14:55 -0600

    feat: Validación de datos

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,27 +2,58 @@ import java.util.Scanner;
 
 public class Calculadora {
     public static void main(String[] args) throws Exception {
-        Scanner numeroTerminal = new Scanner(System.in);
-        Scanner operacionTerminal = new Scanner(System.in);
+        Scanner valorTerminal = new Scanner(System.in);
 
-        System.out.print("Primer número: ");
-        double primerNum = numeroTerminal.nextDouble();
-        System.out.print("Segundo número: ");
-        double segundoNum = numeroTerminal.nextDouble();
-        System.out.print("Operación: ");
-        String operacion = operacionTerminal.nextLine();
-        numeroTerminal.close();
-        operacionTerminal.close();
+        try{
+            System.out.print("Primer número: ");
+            double primerNum = valorTerminal.nextDouble();
+        
+            System.out.print("Segundo número: ");
+            double segundoNum = valorTerminal.nextDouble();
+        
+            mostrarMenu();
+            int operacion = valorTerminal.nextInt();
+            Operacion tipoOperacion = Operacion.values()[operacion-1];
+            valorTerminal.close();
 
-        double resultadoOperacion = 0;
-        if(operacion.equals("suma"));
-        else if(operacion.equals("resta"));
-        else if(operacion.equals("multiplicacion"));
-        else if(operacion.equals("division"));
-        else if(operacion.equals("modulo"));
-        else if(operacion.equals("potencia"));
-        else System.out.println("Operacion no encontrada");
+            double resultadoOperacion = 0;
+            switch (tipoOperacion) {
+                case Op_SUMA:
+                    //funcion suma
+                    break;
+                case Op_RESTA: 
+                    //funcion resta
+                    break;
+                case Op_MULTIPLICACION:
+                    //funcion multiplicacion
+                    break;
+                case Op_DIVISION:
+                    //funcion division
+                    break;
+                case Op_MODULO:
+                    //funcion modulo
+                    break;
+                case Op_POTENCIA:  
+                    //funcion potencia
+                    break;
+            }
+            System.out.println(resultadoOperacion);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Operacion no encontrada.");
+        }
+        catch(Exception e){
+            System.out.println("Tus datos deben ser números.");
+        }
+    }
 
-        System.out.println(resultadoOperacion);
+    public static void mostrarMenu(){
+            System.out.println("Numero de la Operación: ");
+            System.out.println("1.- Suma");
+            System.out.println("2.- Resta");
+            System.out.println("3.- Multiplicación");
+            System.out.println("4.- División");
+            System.out.println("5.- Módulo");
+            System.out.println("6.- Potencia");
     }
 }
