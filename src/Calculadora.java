e5aacc3e1bf0b48c084e1ddad0695c7ba6da4a15
Samuel Rodriguez <samux550@gmail.com>
2023-10-07 19:09:20 -0600

    feat: Funcionalidad de division agregada
    Se agrego un metodo estatico para realizar una division entre 2 numeros y este se llama desde el main cuando el usuario indica que la operacion de los dos numeros sera una division

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calculadora {
@@ -18,11 +19,22 @@ public class Calculadora {
         if(operacion.equals("suma"));
         else if(operacion.equals("resta"));
         else if(operacion.equals("multiplicacion"));
-        else if(operacion.equals("division"));
+        else if(operacion.equals("division")){
+            resultadoOperacion = calcularDivision(primerNum, segundoNum);
+        }
         else if(operacion.equals("modulo"));
         else if(operacion.equals("potencia"));
         else System.out.println("Operacion no encontrada");
 
         System.out.println(resultadoOperacion);
     }
+
+    public static double calcularDivision(double divisor, double dividendo){
+        double resultadoDivision;
+       if(dividendo>0){
+            resultadoDivision = divisor/dividendo;
+            return resultadoDivision;
+       }
+       return 0;
+}
 }
